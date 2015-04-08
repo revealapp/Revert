65a76a3b112eb4424a2cfb699dd7da6b478b7d22
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-08 12:33:07 +1000

    Implementation of simplistic UIPageView

---
+++ Revert/Sources/UIColor+Revert.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  class func peterRiverColor() -> UIColor {
+    return UIColor(red: 52.0 / 255.0, green: 152.0 / 255.0, blue: 219.0 / 255.0, alpha: 1.0)
+  }
+
+  class func wetAsphaltColor() -> UIColor {
+    return UIColor(red: 52.0 / 255.0, green: 73.0 / 255.0, blue: 94.0 / 255.0, alpha: 1.0)
+  }
+
+  class func alizarinColor() -> UIColor {
+    return UIColor(red: 231.0 / 255.0, green: 76.0 / 255.0, blue: 60.0 / 255.0, alpha: 1.0)
+  }
+}
---
+++ Revert/Sources/UIViewController+Revert.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  
+  convenience init(backgroundColor: UIColor) {
+    self.init()
+    self.view.backgroundColor = backgroundColor
+  }
+}
