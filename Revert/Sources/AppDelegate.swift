64adf0c4e8f7c92ebcd1526f8f122a9f7d281d00
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-08 10:38:59 +1000

    Initial code import

---
+++ Revert/Sources/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+  var window: UIWindow?
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    self.dynamicType.configureSplitViewController(self.window!.rootViewController as! UISplitViewController, delegate: self)
+    return true
+  }
+  
+  private class func configureSplitViewController(splitViewController: UISplitViewController, delegate: UISplitViewControllerDelegate) {
+    let navigationController = splitViewController.viewControllers.last as! UINavigationController
+    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
+    splitViewController.delegate = delegate
+  }
+}
+
+// MARK: - UISplitViewControllerDelegate
+
+extension AppDelegate: UISplitViewControllerDelegate {
+  func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController!, ontoPrimaryViewController primaryViewController:UIViewController!) -> Bool {
+    return secondaryViewController as? UINavigationController != nil;
+  }
+}
+
