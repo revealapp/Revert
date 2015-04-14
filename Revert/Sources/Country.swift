a81bcd13cbe27fef29bf9465619d7935c89f1bcd
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-14 13:28:01 +1000

    Set Capital as Optional
    * Considering some 'Countries' (i.e Antartica) don't have capitals.

--- Revert/Sources/Country.swift
+++ Revert/Sources/Country.swift
@@ -6,10 +6,10 @@ import Foundation
 
 struct Country: Collectable {
   let name: String
-  let capital: String
+  let capital: String?
   
   init(dictionary: [String: AnyObject]) {
     self.name = dictionary["name"] as! String
-    self.capital = dictionary["capital"] as! String
+    self.capital = dictionary["capital"] as? String
   }
 }
