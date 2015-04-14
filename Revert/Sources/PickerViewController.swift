4adbc643716dc4192d2c369354763ad60924b8cf
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-14 16:21:41 +1000

    Integrating PickerViewController

---
+++ Revert/Sources/PickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+class PickerViewController: UIViewController {
+  private let collection = CollectableCollection<Country>(resourceFilename: "CountriesCapitals")
+}
+
+// MARK: UIPickerViewDataSource
+
+extension PickerViewController: UIPickerViewDataSource {
+  func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return self.collection[component].countOfRows
+  }
+}
+
+// MARK: UIPickerViewDelegate
+
+extension PickerViewController: UIPickerViewDelegate {
+  func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+    return self.collection.groups.first![row].name
+  }
+}
