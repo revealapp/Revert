07c3ddda201a37416c0248b96cf0473a3f03d91b
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-14 11:17:09 +1000

    Refactored CountryViewController in its own DataSource

---
+++ Revert/Sources/CountryDataSource.swift
@@ -0,0 +1,46 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+class CountryDataSource : NSObject, UITableViewDataSource {
+  private let collection: CollectableCollection<Country>
+  private let cellConfigurator: CountryCellConfigurator
+  
+  required init(collection: CollectableCollection<Country>, cellConfigurator: CountryCellConfigurator) {
+    self.collection = collection
+    self.cellConfigurator = cellConfigurator
+    
+    super.init()
+  }
+  
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return self.collection.countOfGroups
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.collection[section].countOfRows
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier(SB.Cell.TableViewController) as! UITableViewCell
+    let country = self.collection.itemAtIndexPath(indexPath)
+    
+    self.cellConfigurator.configureCell(cell, withCountry: country)
+    return cell
+  }
+  
+  func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return self.collection[section].title
+  }
+  
+  func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+    let count = self.collection[section].countOfRows
+    return NSString(format: NSLocalizedString("tableviewcontroller.footer", comment: "TableViewController footer title"), count) as String
+  }
+  
+  func sectionIndexTitlesForTableView(tableView: UITableView) -> [AnyObject]! {
+    return self.collection.groups.map({$0.title!})
+  }
+}
--- Revert/Sources/TableViewController.swift
+++ Revert/Sources/TableViewController.swift
@@ -5,40 +5,20 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-  private let countries = CollectableCollection<Country>(resourceFileName: "CountriesCapitals")
+  private let collection = CollectableCollection<Country>(resourceFileName: "CountriesCapitals")
   private let cellConfigurator = CountryCellConfigurator()
-}
-
-// MARK : UITableViewDataSource
-
-extension TableViewController: UITableViewDataSource {
-  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return self.countries.countOfGroups
-  }
+  private let dataSource: CountryDataSource
   
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.countries[section].countOfRows
-  }
-  
-  override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return self.countries[section].title
-  }
-  
-  override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-    let count = self.countries[section].countOfRows
-    return NSString(format: NSLocalizedString("tableviewcontroller.footer", comment: "TableViewController footer title"), count) as String
-  }
-  
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier(SB.Cell.TableViewController) as! UITableViewCell
-    let country = self.countries.itemAtIndexPath(indexPath)
+  required init!(coder aDecoder: NSCoder!) {
+    self.dataSource = CountryDataSource(collection: self.collection, cellConfigurator: self.cellConfigurator)
     
-    self.cellConfigurator.configureCell(cell, withCountry: country)
-    return cell
+    super.init(coder: aDecoder)
   }
   
-  override func sectionIndexTitlesForTableView(tableView: UITableView) -> [AnyObject]! {
-    return self.countries.groups.map({$0.title!})
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.tableView.dataSource = self.dataSource
   }
 }
 
