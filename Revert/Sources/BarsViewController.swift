f7f1852c9c078960a461fdce7b37074e89eb1bc9
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-21 12:34:19 +1000

    Removed TabBar from BarsViewController.

--- Revert/Sources/BarsViewController.swift
+++ Revert/Sources/BarsViewController.swift
@@ -6,14 +6,10 @@ import UIKit
 
 class BarsViewController: UIViewController {
   @IBOutlet weak var searchBar: UISearchBar!
-  @IBOutlet weak var tabBar: UITabBar!
   @IBOutlet weak var dismissContainerView: UIView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    // Select TabBar's first item
-    self.tabBar.selectedItem = self.tabBar.items!.first! as? UITabBarItem
 
     // Setup Dismiss Tap Gesture
     let dismissKeyboardGestureRecogniser = UITapGestureRecognizer(target: self, action: "viewTapped:")
