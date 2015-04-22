c7424b5f5d8cc17b172431b8e5ba936aadc9c337
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-22 10:01:18 +1000

    Removed dead code.

--- Revert/Sources/MasterItem.swift
+++ Revert/Sources/MasterItem.swift
@@ -9,15 +9,11 @@ struct MasterItem: Collectable {
   internal let subtitle: String
   internal let iconName: String
   internal let segueIdentifier: String
-  internal let isPush: Bool
-  internal let resourceFilename: String?
   
   init(dictionary: [String: AnyObject]) {
     self.title = dictionary["title"] as! String
     self.subtitle = dictionary["subtitle"] as! String
     self.iconName = dictionary["iconName"] as! String
     self.segueIdentifier = dictionary["segueIdentifier"] as! String
-    self.resourceFilename = dictionary["resourceFilename"] as? String
-    self.isPush = dictionary["isPush"] as! Bool
   }
 }
