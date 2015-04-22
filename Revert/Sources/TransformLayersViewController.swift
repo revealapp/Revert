48472218c04f89ad8979c75ff27def8c2330f016
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-22 14:32:38 +1000

    Added a couple of animations for layertransform

--- Revert/Sources/TransformLayersViewController.swift
+++ Revert/Sources/TransformLayersViewController.swift
@@ -5,7 +5,20 @@
 import UIKit
 
 class TransformLayersViewController: UIViewController {
+  @IBOutlet weak var translateView: UIView!
+  @IBOutlet weak var rotateView: UIView!
+  @IBOutlet weak var scaleView: UIView!
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    self.animateViews()
+  }
   
   
-  
+  private func animateViews() {
+    UIView.animateWithDuration(1.0, delay: 0.0, options: .CurveEaseInOut, animations: { () -> Void in
+      self.rotateView.layer.transform = CATransform3DMakeRotation(CGFloat(30.0 * M_PI / 180.0), 1.0, 0.0, 0.0)
+    }, completion: nil)
+  }
 }
