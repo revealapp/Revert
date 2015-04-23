40f2664312d2527f91e52912d7262871963887b7
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-23 11:25:13 +1000

    WIP. Current issue is with the second rotation.
    * Cube sides keep on disapearing after the second rotation. I'm expecting it to be layout differently than whats shown in reveal (due to the perspective).

--- Revert/Sources/TransformLayersViewController.swift
+++ Revert/Sources/TransformLayersViewController.swift
@@ -5,20 +5,45 @@
 import UIKit
 
 class TransformLayersViewController: UIViewController {
-  @IBOutlet weak var translateView: UIView!
-  @IBOutlet weak var rotateView: UIView!
-  @IBOutlet weak var scaleView: UIView!
+  @IBOutlet weak var yRotateView: UIView!
+  @IBOutlet weak var xRotateView: UIView!
   
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-    
-    self.animateViews()
+  @IBOutlet weak var bottomView: UIView!
+  
+  @IBOutlet weak var cubeFrontView: UIView!
+  @IBOutlet weak var cubeRightView: UIView!
+  @IBOutlet weak var cubeTopView: UIView!
+  
+  private var sideLength: CGFloat {
+    return self.cubeFrontView.bounds.width
   }
   
+  private func degreesToRadians(degrees: CGFloat) -> CGFloat {
+    return degrees * CGFloat(M_PI) / 180.0
+  }
   
-  private func animateViews() {
-    UIView.animateWithDuration(1.0, delay: 0.0, options: .CurveEaseInOut, animations: { () -> Void in
-      self.rotateView.layer.transform = CATransform3DMakeRotation(CGFloat(30.0 * M_PI / 180.0), 1.0, 0.0, 0.0)
-    }, completion: nil)
+  private func transformCube() {
+    let ninetyDegreesInRadian = self.degreesToRadians(90.0)
+    let fortyFiveDegreesInRadian = self.degreesToRadians(30.0)
+    let halfSideLength = self.sideLength / 2.0
+    
+    var rightTransform = CATransform3DMakeTranslation(halfSideLength, 0.0, halfSideLength)
+    rightTransform = CATransform3DRotate(rightTransform, ninetyDegreesInRadian, 0.0, 1.0, 0.0)
+    self.cubeRightView.layer.transform = rightTransform
+    
+    var topTransform = CATransform3DMakeTranslation(0.0, -halfSideLength, -halfSideLength)
+    topTransform = CATransform3DRotate(topTransform, ninetyDegreesInRadian, 1.0, 0.0, 0.0)
+    self.cubeTopView.layer.transform = topTransform
+    
+    var frontTransform = CATransform3DMakeTranslation(0.0, 0.0, -self.sideLength)
+    self.cubeFrontView.layer.transform = frontTransform
+    
+    self.bottomView.layer.transform = CATransform3DMakeRotation(-fortyFiveDegreesInRadian, 0.0, 1.0, 0.0)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.transformCube()
   }
 }
