cc5b82131620c8243d8c2bb33b35ebdb8b01b7a8
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-23 11:45:36 +1000

    Top/Bottom Views animation.

--- Revert/Sources/TransformLayersViewController.swift
+++ Revert/Sources/TransformLayersViewController.swift
@@ -18,32 +18,54 @@ class TransformLayersViewController: UIViewController {
     return self.cubeFrontView.bounds.width
   }
   
+  private var isFirstAppear = true
+  
   private func degreesToRadians(degrees: CGFloat) -> CGFloat {
     return degrees * CGFloat(M_PI) / 180.0
   }
   
-  private func transformCube() {
+  private func buildCube() {
     let ninetyDegreesInRadian = self.degreesToRadians(90.0)
-    let fortyFiveDegreesInRadian = self.degreesToRadians(30.0)
     let halfSideLength = self.sideLength / 2.0
     
     var rightTransform = CATransform3DMakeTranslation(halfSideLength, 0.0, halfSideLength)
     rightTransform = CATransform3DRotate(rightTransform, ninetyDegreesInRadian, 0.0, 1.0, 0.0)
     self.cubeRightView.layer.transform = rightTransform
     
-    var topTransform = CATransform3DMakeTranslation(0.0, -halfSideLength, -halfSideLength)
+    var topTransform = CATransform3DMakeTranslation(0.0, -halfSideLength, halfSideLength)
     topTransform = CATransform3DRotate(topTransform, ninetyDegreesInRadian, 1.0, 0.0, 0.0)
     self.cubeTopView.layer.transform = topTransform
     
-    var frontTransform = CATransform3DMakeTranslation(0.0, 0.0, -self.sideLength)
+    var frontTransform = CATransform3DMakeTranslation(0.0, 0.0, self.sideLength)
     self.cubeFrontView.layer.transform = frontTransform
-    
-    self.bottomView.layer.transform = CATransform3DMakeRotation(-fortyFiveDegreesInRadian, 0.0, 1.0, 0.0)
+  }
+  
+  private func animateIfNecessarry() {
+    if self.isFirstAppear {
+      self.isFirstAppear = false
+
+      let fortyFiveDegreesInRadian = self.degreesToRadians(45.0)
+
+      UIView.animateWithDuration(1.0, delay: 0.0, options: .CurveEaseInOut, animations: {
+        // Top Views
+        self.xRotateView.layer.transform = CATransform3DMakeRotation(fortyFiveDegreesInRadian, 1.0, 0.0, 0.0)
+        self.yRotateView.layer.transform = CATransform3DMakeRotation(fortyFiveDegreesInRadian, 0.0, 1.0, 0.0)
+
+        // Cube
+        self.bottomView.layer.transform = CATransform3DMakeRotation(-fortyFiveDegreesInRadian, 0.0, 1.0, 0.0)
+      }, completion: nil)
+    }
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    self.transformCube()
+   
+    self.buildCube()
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+   
+    self.animateIfNecessarry()
   }
 }
