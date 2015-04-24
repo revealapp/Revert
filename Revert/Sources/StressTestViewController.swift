32ce43ab155b434580d0db29e3463a37f7c8ecf2
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-24 10:48:48 +1000

    StressViewController
    * Updated colours to match designs.
    * Improvements to make sure the view works and transitions properly between portrait / landscape.
    * Added containerView to stressviewcontroller.

--- Revert/Sources/StressTestViewController.swift
+++ Revert/Sources/StressTestViewController.swift
@@ -6,7 +6,23 @@ import UIKit
 
 class StressTestViewController: UIViewController {
 
+  @IBOutlet weak var containerView: UIView!
+  
   private var wasSetup = false
+  private var colors: [UIColor] {
+    return [UIColor.revertBlueColor(), UIColor.revertOrangeColor(), UIColor.revertPinkColor()]
+  }
+  
+  private func colorForIndex(index: Int) -> UIColor {
+    if index % 5 == 0 {
+      return self.colors[(index / 5) % 3]
+    }
+    return UIColor.whiteColor()
+  }
+  
+  private var maximumLayoutSizeForAnyOrientation: CGFloat {
+    return self.tabBarController!.tabBar.bounds.height * 2.0
+  }
 
   private func setupPyramidSubviewsIfNecessary() {
     // Only perform this action once
@@ -16,34 +32,38 @@ class StressTestViewController: UIViewController {
     self.wasSetup = true
     
     let spacing: CGFloat = 5.0
-    let doubleSpacing = spacing * 2.0
-    var currentView = self.view
-    var width = min(currentView.bounds.height, currentView.bounds.width) - self.topLayoutGuide.length - self.bottomLayoutGuide.length
+    var currentView = self.containerView
+    var width = min(self.view.bounds.height, self.view.bounds.width) - self.maximumLayoutSizeForAnyOrientation
     
-    while width > doubleSpacing {
+    for var i = 0; width > 2.0 * spacing; width -= spacing + spacing {
       let subView = UIView()
-      let bindingViews = ["subView": subView]
-      
-      subView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(doubleSpacing / (width - doubleSpacing))
+      subView.backgroundColor = self.colorForIndex(i).colorWithAlphaComponent(0.5)
       subView.setTranslatesAutoresizingMaskIntoConstraints(false)
+      
       currentView.addSubview(subView)
-      
-      let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-        "H:|-\(spacing)-[subView]-\(spacing)-|",
-        options: NSLayoutFormatOptions(0),
-        metrics: nil,
-        views: bindingViews) as! [NSLayoutConstraint]
-      let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
-        "V:|-\(spacing)-[subView]-\(spacing)-|",
-        options: NSLayoutFormatOptions(0),
-        metrics: nil,
-        views: bindingViews) as! [NSLayoutConstraint]
-      
-      currentView.addConstraints(horizontalConstraints + verticalConstraints)
+      currentView.addConstraints(self.dynamicType.constraintsForSubView(subView, spacing: spacing))
       currentView = subView
-      width -= doubleSpacing
+      ++i
     }
+
+    currentView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.75)
     self.view.layoutIfNeeded()
+  }
+  
+  private class func constraintsForSubView(subView: UIView, spacing: CGFloat) -> [NSLayoutConstraint] {
+    let bindingViews = ["subView": subView]
+    let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+      "H:|-\(spacing)-[subView]-\(spacing)-|",
+      options: NSLayoutFormatOptions(0),
+      metrics: nil,
+      views: bindingViews) as! [NSLayoutConstraint]
+    let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat(
+      "V:|-\(spacing)-[subView]-\(spacing)-|",
+      options: NSLayoutFormatOptions(0),
+      metrics: nil,
+      views: bindingViews) as! [NSLayoutConstraint]
+
+    return horizontalConstraints + verticalConstraints
   }
   
   override func viewWillAppear(animated: Bool) {
--- Revert/Sources/UIColor+Revert.swift
+++ Revert/Sources/UIColor+Revert.swift
@@ -16,11 +16,24 @@ extension UIColor {
   internal class func alizarinColor() -> UIColor {
     return UIColor(red: 231.0 / 255.0, green: 76.0 / 255.0, blue: 60.0 / 255.0, alpha: 1.0)
   }
+  
   class func whitesmokeColor() -> UIColor {
     return UIColor(red: 254.0 / 255.0, green: 254.0 / 255.0, blue: 254.0 / 255.0, alpha: 1.0)
   }
   
   class func awesgreenColor() -> UIColor {
     return UIColor(red: 46.0 / 255.0, green: 213.0 / 255.0, blue: 195.0 / 255.0, alpha: 1.0)
+  }
+  
+  class func revertBlueColor() -> UIColor {
+    return UIColor(red: 0.231, green: 0.396, blue: 1.000, alpha: 1.0)
+  }
+  
+  class func revertOrangeColor() -> UIColor {
+    return UIColor(red: 0.992, green: 0.663, blue: 0.161, alpha: 1.0)
+  }
+  
+  class func revertPinkColor() -> UIColor {
+    return UIColor(red: 0.804, green: 0.357, blue: 0.988, alpha: 1.0)
   }
 }
