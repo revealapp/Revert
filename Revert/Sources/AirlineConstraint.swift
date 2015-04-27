387b7431e8acee9bcf183b818a2291f285ddb833
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-27 12:22:55 +1000

    Fixed 1px high / wide view.

---
+++ Revert/Sources/AirlineConstraint.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+class AirlineConstraint: NSLayoutConstraint {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    self.constant = self.dynamicType.constantValue
+  }
+  
+  private class var constantValue: CGFloat {
+    return 1.0 / UIScreen.mainScreen().scale
+  }
+}
