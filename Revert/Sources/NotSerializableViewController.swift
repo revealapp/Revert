184e2ee2f046a9e9ab4e899b02dcaf9625f9ed73
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-27 15:44:07 +1000

    Inserted Non Serializable View Controller.

---
+++ Revert/Sources/NotSerializableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+class NonSerializableViewController: UIViewController {
+  
+  @IBOutlet weak var progressView: UIProgressView!
+  @IBOutlet weak var subView: UIView!
+  @IBOutlet weak var slider: UISlider!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // inf
+    self.slider.maximumValue = 1.0 / 0.0
+    
+    // nan
+    self.progressView.contentScaleFactor = 0.0 / 0.0
+    
+    // - inf
+    self.subView.contentScaleFactor = -1.0 / 0.0
+  }
+}
