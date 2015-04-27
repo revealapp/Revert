442f17db1060f89605e4ab8f21f1a2e982e22ad2
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-27 16:21:30 +1000

    Added alert / activity indicator for WebView VC.

--- Revert/Sources/WebViewController.swift
+++ Revert/Sources/WebViewController.swift
@@ -7,14 +7,20 @@ import WebKit
 
 class WebViewController: UIViewController {
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    let webView = self.dynamicType.loadedWebView()
-    webView.setTranslatesAutoresizingMaskIntoConstraints(false)
-    self.view.addSubview(webView)
-    self.view.addConstraints(self.dynamicType.constraintsForWebView(webView))
-  }
+  private lazy var webView: UIView = {
+    let req = NSURLRequest(URL: NSURL(string: "http://www.revealapp.com")!)
+    if let _ = NSClassFromString("WKWebView") {
+      let webView = WKWebView()
+      webView.loadRequest(req)
+      webView.navigationDelegate = self
+      return webView
+    } else {
+      let webView = UIWebView()
+      webView.loadRequest(req)
+      webView.delegate = self
+      return webView
+    }
+  }()
   
   private class func constraintsForWebView(webView: UIView) -> [NSLayoutConstraint] {
     let bindingViews = ["webView": webView]
@@ -32,17 +38,52 @@ class WebViewController: UIViewController {
     return horizontalConstraints + verticalConstraints
   }
   
-  private class func loadedWebView() -> UIView {
-    let req = NSURLRequest(URL: NSURL(string: "http://www.revealapp.com")!)
+  private func showFailAlert() {
+    UIAlertView(
+      title: NSLocalizedString("error", comment: "Alert title on content failed loading"),
+      message: NSLocalizedString("webviewcontroller.alert.message", comment: "Alert message on content failed loading"),
+      delegate: nil,
+      cancelButtonTitle: NSLocalizedString("ok", comment: "Alert dismiss button on content failed loading")
+    ).show()
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    if let _ = NSClassFromString("WKWebView") {
-      let webView = WKWebView()
-      webView.loadRequest(req)
-      return webView
-    } else {
-      let webView = UIWebView()
-      webView.loadRequest(req)
-      return webView
-    }
+    self.webView.setTranslatesAutoresizingMaskIntoConstraints(false)
+    self.view.addSubview(self.webView)
+    self.view.addConstraints(self.dynamicType.constraintsForWebView(self.webView))
+  }
+}
+
+// MARK: UIWebViewDelegate
+extension WebViewController: UIWebViewDelegate {
+  func webViewDidStartLoad(webView: UIWebView) {
+    UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+  }
+  
+  func webViewDidFinishLoad(webView: UIWebView) {
+    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+  }
+}
+
+// MARK: WKNavigationDelegate
+extension WebViewController: WKNavigationDelegate {
+  func webView(webView: WKWebView, didCommitNavigation navigation: WKNavigation!) {
+    UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+  }
+  
+  func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+  }
+  
+  func webView(webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: NSError) {
+    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    self.showFailAlert()
+  }
+  
+  func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
+    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    self.showFailAlert()
   }
 }
