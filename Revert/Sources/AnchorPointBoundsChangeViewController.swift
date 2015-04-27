0349923831ab869ec441d006cd9a25047a098655
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-27 13:47:52 +1000

    Added bounds testing & anchor point examples

---
+++ Revert/Sources/AnchorPointBoundsChangeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+class AnchorPointBoundsChangeViewController: UIViewController {
+  @IBOutlet weak var boundsChangeView: AirlineBorderView!
+  @IBOutlet weak var anchorPointView: AirlineBorderView!
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    self.animateIfNecessary()
+  }
+  
+  private var wasAnimated = false
+  
+  private func animateIfNecessary() {
+    if self.wasAnimated {
+      return
+    }
+    self.wasAnimated = true
+    
+    // Bounds Change testing
+    let offset: CGFloat = -25.0
+    UIView.animateWithDuration(1.0, delay: 0.0, options: .CurveEaseInOut, animations: { () -> Void in
+      self.boundsChangeView.bounds = CGRectOffset(self.boundsChangeView.bounds, offset, offset)
+    }, completion: nil)
+    
+    // Anchor Point Testing
+    let toValue = CGPoint(x: 0.25, y: 0.25)
+    let basicAnimation = CABasicAnimation(keyPath: "anchorPoint")
+    basicAnimation.duration = 1.0
+    basicAnimation.fromValue = NSValue(CGPoint: self.anchorPointView.layer.anchorPoint)
+    basicAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+    basicAnimation.toValue = NSValue(CGPoint: toValue)
+    self.anchorPointView.layer.addAnimation(basicAnimation, forKey: "anchorPoint")
+    self.anchorPointView.layer.anchorPoint = toValue
+  }
+}
