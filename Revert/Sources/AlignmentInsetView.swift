065367934607f2ab7aff14b9e8f55fe5825cabc8
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-27 12:50:17 +1000

    Added alignmentInsets example.

---
+++ Revert/Sources/AlignmentInsetView.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+class AlignmentInsetView: UIView {
+  override func alignmentRectInsets() -> UIEdgeInsets {
+    return UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+  }
+}
