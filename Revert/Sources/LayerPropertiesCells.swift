4b6f00c752e855d4e6945e42a855e4b830f0633e
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-28 12:39:09 +1000

    CATextLayer implementation

--- Revert/Sources/LayerPropertiesCells.swift
+++ Revert/Sources/LayerPropertiesCells.swift
@@ -7,6 +7,21 @@ import UIKit
 class CATextLayerCell: UICollectionViewCell {
   
   @IBOutlet weak var textLayerView: CATextLayerView!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    self.dynamicType.configureTextLayer(self.textLayerView.textLayer)
+  }
+  
+  private class func configureTextLayer(textLayer: CATextLayer) {
+    textLayer.string = "I am a CATextLayer string. I'm quite long and wordy."
+    textLayer.fontSize = 20.0
+    textLayer.foregroundColor = UIColor.lightGrayColor().CGColor
+    textLayer.alignmentMode = kCAAlignmentCenter
+    textLayer.truncationMode = kCATruncationMiddle
+    textLayer.wrapped = true
+  }
 }
 
 class CAEmitterLayerCell: UICollectionViewCell {
