cf93b5978895ee2bbefa8cebf2393e03831a0a8d
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-28 11:44:53 +1000

    First take on layerProperties

---
+++ Revert/Sources/LayerPropertiesCells.swift
@@ -0,0 +1,36 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+class CATextLayerCell: UICollectionViewCell {
+  
+}
+
+class CAEmitterLayerCell: UICollectionViewCell {
+  
+}
+
+class CAShapeLayerCell: UICollectionViewCell {
+  
+}
+
+class CATiledLayerCell: UICollectionViewCell {
+  
+}
+
+class CAGradientLayerCell: UICollectionViewCell {
+  
+}
+
+class CAReplicatorLayerCell: UICollectionViewCell {
+  
+}
+
+class CAAEGLayerCell: UICollectionViewCell {
+  
+}
+
+
+
---
+++ Revert/Sources/LayerViews.swift
@@ -0,0 +1,85 @@
+//
+//  Copyright (c) 2015 Itty Bitty Apps. All rights reserved.
+//
+
+import UIKit
+
+class CATextLayerView: UIView {
+  override class func layerClass() -> AnyClass {
+    return CATextLayer.self
+  }
+  
+  var textLayer: CATextLayer {
+    return self.layer as! CATextLayer
+  }
+}
+
+class CAScrollLayerView: UIView {
+  override class func layerClass() -> AnyClass {
+    return CAScrollLayer.self
+  }
+  
+  var scrollLayer: CAScrollLayer {
+    return self.layer as! CAScrollLayer
+  }
+}
+
+class CAEmitterLayerView: UIView {
+  override class func layerClass() -> AnyClass {
+    return CAEmitterLayer.self
+  }
+  
+  var emitterLayer: CAEmitterLayer {
+    return self.layer as! CAEmitterLayer
+  }
+}
+
+class CAShapeLayerView: UIView {
+  override class func layerClass() -> AnyClass {
+    return CAShapeLayer.self
+  }
+  
+  var shapeLayer: CAShapeLayer {
+    return self.layer as! CAShapeLayer
+  }
+}
+
+class CATiledLayerView: UIView {
+  override class func layerClass() -> AnyClass {
+    return CATiledLayer.self
+  }
+  
+  var tiledLayer: CATiledLayer {
+    return self.layer as! CATiledLayer
+  }
+}
+
+class CAGradientLayerView: UIView {
+  override class func layerClass() -> AnyClass {
+    return CAGradientLayer.self
+  }
+  
+  var gradientLayer: CAGradientLayer {
+    return self.layer as! CAGradientLayer
+  }
+}
+
+class CAReplicatorLayerView: UIView {
+  override class func layerClass() -> AnyClass {
+    return CAReplicatorLayer.self
+  }
+  
+  var replicatorLayer: CAReplicatorLayer {
+    return self.layer as! CAReplicatorLayer
+  }
+}
+
+class CAAEGLayerView: UIView {
+  override class func layerClass() -> AnyClass {
+    return CATextLayer.self
+  }
+  
+  var textLayer: CATextLayer {
+    return self.layer as! CATextLayer
+  }
+}
