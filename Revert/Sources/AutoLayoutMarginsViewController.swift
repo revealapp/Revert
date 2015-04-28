5e878947869bac324f06b106f25606e56fe61734
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-28 23:35:29 +1000

    ALMargins - Swapped UIStepper with UISlider.
    * Added final class declarations.

--- Revert/Sources/AutoLayoutMarginsViewController.swift
+++ Revert/Sources/AutoLayoutMarginsViewController.swift
@@ -4,24 +4,26 @@
 
 import UIKit
 
-class AutoLayoutMarginsViewController: UIViewController {
-  @IBOutlet weak var stepper: UIStepper!
+final class CustomIntrinsicContentSizeView: UIView {
+  override func intrinsicContentSize() -> CGSize {
+    return CGSize(width: 80.0, height: 80.0)
+  }
+}
+
+final class AutoLayoutMarginsViewController: UIViewController {
   @IBOutlet weak var centerView: UIView!
+  @IBOutlet weak var slider: UISlider!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    self.stepper.value = Double(self.centerView.layoutMargins.top)
+    self.slider.minimumValue = Float(self.centerView.layoutMargins.top)
+    self.slider.value = self.slider.minimumValue
+    self.slider.maximumValue = self.slider.minimumValue + 100.0
   }
   
-  @IBAction func stepperChanged(sender: UIStepper) {
+  @IBAction func sliderValueChanged(sender: UISlider) {
     let margin = CGFloat(sender.value)
     self.centerView.layoutMargins = UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
-  }
-}
-
-class CustomIntrinsicContentSizeView: UIView {
-  override func intrinsicContentSize() -> CGSize {
-    return CGSize(width: 80.0, height: 80.0)
   }
 }
