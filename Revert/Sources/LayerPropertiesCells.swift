6e0272879a4959787a795955d385cd6c7c8cacd8
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-04-28 12:04:32 +1000

    IBOutlet connections
    + adding missing ScrollLayerCell

--- Revert/Sources/LayerPropertiesCells.swift
+++ Revert/Sources/LayerPropertiesCells.swift
@@ -6,30 +6,42 @@ import UIKit
 
 class CATextLayerCell: UICollectionViewCell {
   
+  @IBOutlet weak var textLayerView: CATextLayerView!
 }
 
 class CAEmitterLayerCell: UICollectionViewCell {
   
+  @IBOutlet weak var emitterLayerView: CAEmitterLayerView!
 }
 
 class CAShapeLayerCell: UICollectionViewCell {
   
+  @IBOutlet weak var shapeLayerView: CAShapeLayerView!
+}
+
+class CAScrollLayerCell: UICollectionViewCell {
+  
+  @IBOutlet weak var scrollLayerView: CAScrollLayerView!
 }
 
 class CATiledLayerCell: UICollectionViewCell {
   
+  @IBOutlet weak var tiledLayerView: CATiledLayerView!
 }
 
 class CAGradientLayerCell: UICollectionViewCell {
   
+  @IBOutlet weak var gradientLayerView: CAGradientLayerView!
 }
 
 class CAReplicatorLayerCell: UICollectionViewCell {
   
+  @IBOutlet weak var replicatorLayerView: CAReplicatorLayerView!
 }
 
 class CAAEGLayerCell: UICollectionViewCell {
   
+  @IBOutlet weak var aegLayerCell: CAAEGLayerView!
 }
 
 
