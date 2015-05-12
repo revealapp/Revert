8b905ec33322dbfada3df289498f64dd959779da
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-05-12 11:06:38 +1000

    Info for BarsViewController.

--- Revert/Sources/BarsViewController.swift
+++ Revert/Sources/BarsViewController.swift
@@ -4,19 +4,26 @@
 
 import UIKit
 
-class BarsViewController: UIViewController {
+final internal class BarsViewController: UIViewController, SettableMasterItem {
+  var item: MasterItem?
   @IBOutlet weak var searchBar: UISearchBar!
   @IBOutlet weak var dismissContainerView: UIView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    assert(self.item != nil, "Item must be set before `viewDidLoad`")
+    
     // Setup Dismiss Tap Gesture
     let dismissKeyboardGestureRecogniser = UITapGestureRecognizer(target: self, action: "viewTapped:")
     self.dismissContainerView.addGestureRecognizer(dismissKeyboardGestureRecogniser)
   }
   
-  internal func viewTapped(gestureRecogniser: UITapGestureRecognizer) {
+  func viewTapped(gestureRecogniser: UITapGestureRecognizer) {
     self.searchBar.resignFirstResponder()
+  }
+  
+  @IBAction func infoButtonTapped(sender: UIBarButtonItem) {
+    self.presentInfoViewControllerWithItem(self.item!)
   }
 }
