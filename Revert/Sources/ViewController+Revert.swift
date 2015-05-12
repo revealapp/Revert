80f76da2f65b8de149b74a208e7e705c033ffe41
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-05-12 22:30:28 +1000

    Removed Popover from Pad indiom

--- Revert/Sources/ViewController+Revert.swift
+++ Revert/Sources/ViewController+Revert.swift
@@ -5,18 +5,9 @@
 import UIKit
 
 extension UIViewController {
-  
   internal func presentInfoViewControllerWithItem(item: MasterItem) {
     let infoNavigationController = self.storyboard!.instantiateViewControllerWithIdentifier(SB.VC.Help) as! UINavigationController
-    
     (infoNavigationController.topViewController as! InfoViewController).item = item
-
-    if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-      let popover = UIPopoverController(contentViewController: infoNavigationController)
-      popover.backgroundColor = UIColor.revertTintColor()
-      popover.presentPopoverFromBarButtonItem(self.navigationItem.rightBarButtonItem!, permittedArrowDirections: .Up, animated: true)
-    } else {
-      self.presentViewController(infoNavigationController, animated: true, completion: nil)
-    }
+    self.presentViewController(infoNavigationController, animated: true, completion: nil)
   }
 }
