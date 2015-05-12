fea88650e12b1b20693a131805a0bdc1435cfb28
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-05-12 11:37:41 +1000

    Info for AutoLayoutMarginsViewController.

--- Revert/Sources/AutoLayoutMarginsViewController.swift
+++ Revert/Sources/AutoLayoutMarginsViewController.swift
@@ -4,37 +4,31 @@
 
 import UIKit
 
-final class CustomIntrinsicContentSizeView: UIView {
+final internal class CustomIntrinsicContentSizeView: UIView {
   override func intrinsicContentSize() -> CGSize {
     return CGSize(width: 80.0, height: 80.0)
   }
 }
 
-final class AutoLayoutMarginsViewController: UIViewController {
+final internal class AutoLayoutMarginsViewController: UIViewController, SettableMasterItem {
   @IBOutlet weak var centerView: UIView!
   @IBOutlet weak var slider: UISlider!
   @IBOutlet weak var centerViewWidthConstraint: NSLayoutConstraint!
   @IBOutlet weak var containerView: UIView!
-  
+  var item: MasterItem?
+
   private let interSquareSpacing: CGFloat = 40
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    assert(self.item != nil, "Item must be set before `viewDidLoad`")
+
     if self.centerView.respondsToSelector("layoutMargins") {
       self.slider.minimumValue = Float(self.centerView.layoutMargins.top)
       self.slider.value = self.slider.minimumValue
       self.slider.maximumValue = self.slider.minimumValue + 100.0
     }
-  }
-  
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-  }
-  
-  override func viewWillLayoutSubviews() {
-    super.viewWillLayoutSubviews()
-   
   }
   
   override func viewDidLayoutSubviews() {
@@ -46,14 +40,17 @@ final class AutoLayoutMarginsViewController: UIViewController {
   
   private var hasDisplayedHelp = false
   
+  @IBAction func infoButtonTapped(sender: UIBarButtonItem) {
+    self.presentInfoViewControllerWithItem(self.item!)
+  }
+  
   @IBAction func sliderValueChanged(sender: UISlider) {
-    
     if self.centerView.respondsToSelector("layoutMargins") {
       let margin = CGFloat(sender.value)
       self.centerView.layoutMargins = UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
     } else if self.hasDisplayedHelp == false {
-      // Display HELP
       self.hasDisplayedHelp = true
+      self.presentInfoViewControllerWithItem(self.item!)
     }
   }
 }
