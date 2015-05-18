d134bc856e1a1a2bac449abbc199fc43863af994
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-05-18 15:33:37 +1000

    Defined default region for MapView.

--- Revert/Sources/MapViewController.swift
+++ Revert/Sources/MapViewController.swift
@@ -12,6 +12,14 @@ final class MapViewController: RevertViewController {
     super.viewDidLoad()
 
     self.addAnnotations()
+    
+    self.mapView.region = self.ozRegion
+  }
+  
+  private var ozRegion: MKCoordinateRegion {
+    let center = CLLocationCoordinate2D(latitude: -24.291451, longitude: 134.126772)
+    let span = MKCoordinateSpan(latitudeDelta: 50, longitudeDelta: 50)
+    return MKCoordinateRegion(center: center, span: span)
   }
   
   private func addAnnotations() {
