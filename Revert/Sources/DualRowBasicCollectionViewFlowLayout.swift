49484f7df06974ea1862da22e9c6c80f2f0de665
Hugo Cuvillier <hugo@ittybittyapps.com>
2015-09-23 21:09:07 +1000

    Makes noOfItemsInRow static member

--- Revert/Sources/DualRowBasicCollectionViewFlowLayout.swift
+++ Revert/Sources/DualRowBasicCollectionViewFlowLayout.swift
@@ -5,10 +5,10 @@
 import UIKit
 
 final class DualRowBasicCollectionViewFlowLayout: UICollectionViewFlowLayout {
-  private let noOfItemsInRow = 2
+  private static let noOfItemsInRow = 2
   
   private var computedItemSize: CGSize {
-    let itemWidth = floor((self.collectionView!.bounds.width - self.minimumInteritemSpacing - self.sectionInset.left - self.sectionInset.right) / CGFloat(self.noOfItemsInRow))
+    let itemWidth = floor((self.collectionView!.bounds.width - self.minimumInteritemSpacing - self.sectionInset.left - self.sectionInset.right) / CGFloat(self.dynamicType.noOfItemsInRow))
     return CGSize(width: itemWidth, height: self.itemSize.height)
   }
   
