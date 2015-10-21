a189279d6e435affdfef4cac0d62b35d3a578841
Tony Arnold <tony@thecocoabots.com>
2015-10-22 10:09:13 +1100

    Quote paths and see if removing `set -e` lets the script run under CI

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #/usr/bin/env bash
 
-set -e # Stop script at first error
-
 # Binaries
 
 XCRUN=$(which xcrun)
@@ -11,21 +9,21 @@ RM=$(which rm)
 
 # Project / Folders
 
-DERIVED_DATA_FOLDER=./DerivedData
-PROJECT_FILE=./Revert.xcodeproj
+DERIVED_DATA_FOLDER=DerivedData
+PROJECT_FILE=Revert.xcodeproj
 SCHEME=Revert
 
 # Script
 
 echo 'Cleaning Devived Data Folder...'
 
-$RM -rf $DERIVED_DATA_FOLDER
-$MKDIR $DERIVED_DATA_FOLDER
+$RM -rf "$DERIVED_DATA_FOLDER"
+$MKDIR "$DERIVED_DATA_FOLDER"
 
 BUILD_CMD="$XCRUN xcodebuild -project $PROJECT_FILE -scheme $SCHEME -sdk iphonesimulator clean build -derivedDataPath $DERIVED_DATA_FOLDER"
 
 if [ -x $XCPRETTY ]; then
-  $BUILD_CMD | tee $DERIVED_DATA_FOLDER/build.log | $XCPRETTY -c && exit ${PIPESTATUS[0]}
+  $BUILD_CMD | tee "$DERIVED_DATA_FOLDER/build.log" | $XCPRETTY -c && exit ${PIPESTATUS[0]}
 else
   $BUILD_CMD
 fi
