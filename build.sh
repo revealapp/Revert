0496488326c804e67b6b71b12c8b2e51e1da0078
Tony Arnold <tony@thecocoabots.com>
2015-10-22 10:13:55 +1100

    Remove use of `which` in place of a hash check for xcpretty

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #/usr/bin/env bash
 
-# Binaries
-
-XCRUN=$(which xcrun)
-XCPRETTY=$(which xcpretty)
-MKDIR=$(which mkdir)
-RM=$(which rm)
-
 # Project / Folders
-
 DERIVED_DATA_FOLDER=DerivedData
 PROJECT_FILE=Revert.xcodeproj
 SCHEME=Revert
@@ -17,13 +9,13 @@ SCHEME=Revert
 
 echo 'Cleaning Devived Data Folder...'
 
-$RM -rf "$DERIVED_DATA_FOLDER"
-$MKDIR "$DERIVED_DATA_FOLDER"
+rm -rf "$DERIVED_DATA_FOLDER"
+mkdir "$DERIVED_DATA_FOLDER"
 
-BUILD_CMD="$XCRUN xcodebuild -project $PROJECT_FILE -scheme $SCHEME -sdk iphonesimulator clean build -derivedDataPath $DERIVED_DATA_FOLDER"
+BUILD_CMD="xcrun xcodebuild -project $PROJECT_FILE -scheme $SCHEME -sdk iphonesimulator clean build -derivedDataPath $DERIVED_DATA_FOLDER"
 
-if [ -x $XCPRETTY ]; then
-  $BUILD_CMD | tee "$DERIVED_DATA_FOLDER/build.log" | $XCPRETTY -c && exit ${PIPESTATUS[0]}
+if hash xcpretty 2>/dev/null; then
+  $BUILD_CMD | tee "$DERIVED_DATA_FOLDER/build.log" | xcpretty -c && exit ${PIPESTATUS[0]}
 else
   $BUILD_CMD
 fi
