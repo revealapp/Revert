b74b93a90661f038b2cc7ff28683d7b0f774ff3a
Ben Thomas <ben@ittybittyapps.com>
2015-10-28 15:54:43 +1100

    Calculates the collection view cell sizes based on available space instead of device type, to support iPad split view multi-tasking.

--- Revert/Sources/AdaptiveCollectionViewFlowLayout.swift
+++ Revert/Sources/AdaptiveCollectionViewFlowLayout.swift
@@ -5,17 +5,25 @@
 import UIKit
 
 final class AdaptiveCollectionViewFlowLayout: UICollectionViewFlowLayout {
+  private let minColumnsForLayout = 2
+  private let maxColumnsForLayout = 3
+  private let minCellWidth: CGFloat = 150
+
+  private var cellWidthForCollectionView: CGFloat {
+    let totalPadding = CGFloat(maxColumnsForLayout - 1) * self.minimumInteritemSpacing + self.sectionInset.left + self.sectionInset.right
+    let collectionViewWidth = CGRectGetWidth(self.collectionView!.bounds)
+    return (collectionViewWidth - totalPadding) / CGFloat(maxColumnsForLayout)
+  }
+
   private var noOfItemsInRow: Int {
-    switch UIApplication.sharedApplication().statusBarOrientation {
-    case .Portrait, .PortraitUpsideDown, .Unknown:
-      return 2
-    case .LandscapeRight, .LandscapeLeft:
-      // Scale is important here as size-classes cannot be used in projects compatible with iOS 7. Scale will be @3x
-      // only on iPhone 6+ if and only if the device is not in scaled mode - which is exactly what we want.
-      return UIScreen.mainScreen().scale > 2 ? 2 : 3
+    if cellWidthForCollectionView < minCellWidth {
+      return minColumnsForLayout
+    }
+    else {
+      return maxColumnsForLayout
     }
   }
-  
+
   private var totalItemsWidth: CGFloat {
     guard let collectionView = self.collectionView else {
       return 0
@@ -24,12 +32,12 @@ final class AdaptiveCollectionViewFlowLayout: UICollectionViewFlowLayout {
     let horizontalSectionInsets = self.sectionInset.left + self.sectionInset.right
     return collectionView.bounds.width - horizontalSectionInsets
   }
-  
+
   private var itemWidth: CGFloat {
     let separatorsWidth = (CGFloat(self.noOfItemsInRow - 1) * self.minimumInteritemSpacing)
     return floor((self.totalItemsWidth - separatorsWidth) / CGFloat(self.noOfItemsInRow))
   }
-  
+
   override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
     return newBounds.size != self.collectionView?.bounds.size
   }
