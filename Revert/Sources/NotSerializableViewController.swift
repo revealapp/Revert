7931e340cf508cf5a8bcf7596c70b49526fd0d6c
Tony Arnold <tony@thecocoabots.com>
2015-11-11 14:51:41 +1100

    Show an alert before crashing due to unserializable constraints

--- Revert/Sources/NotSerializableViewController.swift
+++ Revert/Sources/NotSerializableViewController.swift
@@ -11,13 +11,64 @@ final class NonSerializableViewController: RevertViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    self.slider.addTarget(self, action: "warnBeforeCrash:", forControlEvents: .TouchUpInside)
+  }
+
+  func warnBeforeCrash(sender: UISlider?) {
+    let alertTitle = NSLocalizedString("Modifying the UISlider again will cause the app to crash", comment: "Unserializable slider title")
+    let alertMessage = NSLocalizedString("This is intentional, and demonstrates what happens when a property is not serializable.", comment: "Unserializable slider message")
+
+    if #available(iOS 8.0, *) {
+      let alertViewController = UIAlertController(
+        title: alertTitle,
+        message: alertMessage,
+        preferredStyle: .Alert
+      )
+
+      let continueAction = UIAlertAction(
+        title: NSLocalizedString("Ok", comment: "Alert Ok button title"),
+        style: .Default) { [weak self] _ in
+          self?.makeSliderUnserializable(self)
+
+          // Only show the alert once
+          self?.slider.removeTarget(self, action: "warnBeforeCrash:", forControlEvents: .TouchUpInside)
+      }
+
+      alertViewController.addAction(continueAction)
+
+      self.presentViewController(alertViewController, animated: true, completion: nil)
+    } else {
+      let alertView = UIAlertView(
+        title: alertTitle,
+        message: alertMessage,
+        delegate: nil,
+        cancelButtonTitle: NSLocalizedString("OK", comment: "Unserializable slider OK button")
+      )
+
+      alertView.show()
+    }
+
+  }
+
+  private func makeSliderUnserializable(viewController: NonSerializableViewController?) {
     // inf
-    self.slider.maximumValue = 1 / 0
+    viewController?.slider.maximumValue = 1 / 0
 
     // nan
-    self.progressView.contentScaleFactor = 0 / 0
+    viewController?.progressView.contentScaleFactor = 0 / 0
 
     // - inf
-    self.subView.contentScaleFactor = -1 / 0
+    viewController?.subView.contentScaleFactor = -1 / 0
   }
+}
+
+extension NonSerializableViewController: UIAlertViewDelegate {
+
+  func alertView(alertView: UIAlertView, willDismissWithButtonIndex buttonIndex: Int) {
+    makeSliderUnserializable(self)
+
+    // Only show the alert once
+    self.slider.removeTarget(self, action: "warnBeforeCrash:", forControlEvents: .TouchUpInside)
+  }
+
 }
