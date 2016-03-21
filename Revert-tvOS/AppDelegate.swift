f6e8d36e95b50ac21ac51883e8f6bd5177d7ffed
Brunno Ferreira <git-commit@brunnoferreira.org>
2016-03-21 13:18:15 +1100

    Initial setup for tvOS support

---
+++ Revert-tvOS/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright © 2016 Itty Bitty Apps. All rights reserved.
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+
+}
+
