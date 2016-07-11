d505a41c5c9041af71968e44bf02498c17c08e82
Brunno Ferreira <git-commit@brunnoferreira.org>
2016-07-11 17:14:01 +1000

    Fixes code organization

--- Revert/Sources/NotSerializableViewController.swift
+++ Revert/Sources/NotSerializableViewController.swift
@@ -4,52 +4,52 @@
 import UIKit
 
 final class NonSerializableViewController: RevertViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-    self.slider.addTarget(self, action: #selector(self.warnBeforeCrash(_:)), forControlEvents: .TouchUpInside)
-    self.slider.addTarget(self, action: #selector(self.warnBeforeCrash(_:)), forControlEvents: .TouchUpOutside)
-  }
+		self.slider.addTarget(self, action: #selector(self.warnBeforeCrash(_:)), forControlEvents: .TouchUpInside)
+		self.slider.addTarget(self, action: #selector(self.warnBeforeCrash(_:)), forControlEvents: .TouchUpOutside)
+	}
 
-  func warnBeforeCrash(sender: UISlider?) {
-    let alertTitle = NSLocalizedString("Modifying the UISlider again will cause the app to crash", comment: "Unserializable slider title")
-    let alertMessage = NSLocalizedString("This is intentional, and demonstrates what happens when a property is not serializable.", comment: "Unserializable slider message")
+	func warnBeforeCrash(sender: UISlider?) {
+		let alertTitle = NSLocalizedString("Modifying the UISlider again will cause the app to crash", comment: "Unserializable slider title")
+		let alertMessage = NSLocalizedString("This is intentional, and demonstrates what happens when a property is not serializable.", comment: "Unserializable slider message")
 
-    let alertViewController = UIAlertController(
-      title: alertTitle,
-      message: alertMessage,
-      preferredStyle: .Alert
-    )
-    
-    let continueAction = UIAlertAction(
-      title: NSLocalizedString("Ok", comment: "Alert Ok button title"),
-      style: .Default) { [weak self] _ in
-        self?.makeSliderUnserializable()
-        
-        // Only show the alert once
-        self?.slider.removeTarget(self, action: #selector(self?.warnBeforeCrash(_:)), forControlEvents: .TouchUpInside)
-        self?.slider.removeTarget(self, action: #selector(self?.warnBeforeCrash(_:)), forControlEvents: .TouchUpOutside)
-    }
-    
-    alertViewController.addAction(continueAction)
-    
-    self.presentViewController(alertViewController, animated: true, completion: nil)
-  }
+		let alertViewController = UIAlertController(
+			title: alertTitle,
+			message: alertMessage,
+			preferredStyle: .Alert
+		)
 
-  // MARK: Private
+		let continueAction = UIAlertAction(
+			title: NSLocalizedString("Ok", comment: "Alert Ok button title"),
+			style: .Default) { [weak self] _ in
+				self?.makeSliderUnserializable()
 
-  @IBOutlet private weak var progressView: UIProgressView!
-  @IBOutlet private weak var subView: UIView!
-  @IBOutlet private weak var slider: UISlider!
+				// Only show the alert once
+				self?.slider.removeTarget(self, action: #selector(self?.warnBeforeCrash(_:)), forControlEvents: .TouchUpInside)
+				self?.slider.removeTarget(self, action: #selector(self?.warnBeforeCrash(_:)), forControlEvents: .TouchUpOutside)
+		}
 
-  private func makeSliderUnserializable() {
-    // inf
-    self.slider.maximumValue = 1 / 0
+		alertViewController.addAction(continueAction)
 
-    // nan
-    self.progressView.contentScaleFactor = 0 / 0
+		self.presentViewController(alertViewController, animated: true, completion: nil)
+	}
 
-    // - inf
-    self.subView.contentScaleFactor = -1 / 0
-  }
+	// MARK: Private
+
+	@IBOutlet private weak var progressView: UIProgressView!
+	@IBOutlet private weak var subView: UIView!
+	@IBOutlet private weak var slider: UISlider!
+
+	private func makeSliderUnserializable() {
+		// inf
+		self.slider.maximumValue = 1 / 0
+
+		// nan
+		self.progressView.contentScaleFactor = 0 / 0
+
+		// - inf
+		self.subView.contentScaleFactor = -1 / 0
+	}
 }
--- Revert/Sources/WebViewController.swift
+++ Revert/Sources/WebViewController.swift
@@ -5,120 +5,120 @@ import UIKit
 import WebKit
 
 final class WebViewController: RevertViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
+	override func viewDidLoad() {
+		super.viewDidLoad()
 
-    if NSClassFromString("WKWebView") == nil {
-      self.navigationItem.titleView = nil
-      self.navigationItem.title = NSLocalizedString("WebView", comment: "WebView title")
-    }
+		if NSClassFromString("WKWebView") == nil {
+			self.navigationItem.titleView = nil
+			self.navigationItem.title = NSLocalizedString("WebView", comment: "WebView title")
+		}
 
-    self.setupWebView(self.uiWebView)
-  }
+		self.setupWebView(self.uiWebView)
+	}
 
-  override func viewDidDisappear(animated: Bool) {
-    super.viewDidDisappear(animated)
+	override func viewDidDisappear(animated: Bool) {
+		super.viewDidDisappear(animated)
 
-    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-  }
+		UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+	}
 
-  override func viewWillLayoutSubviews() {
-    super.viewWillLayoutSubviews()
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
 
-    self.topConstraint?.constant = self.currentWebView?.isKindOfClass(UIWebView.self) == true ? -self.topLayoutGuide.length : 0
-  }
+		self.topConstraint?.constant = self.currentWebView?.isKindOfClass(UIWebView.self) == true ? -self.topLayoutGuide.length : 0
+	}
 
-  // MARK: Private
+	// MARK: Private
 
-  private enum Type: Int {
-    case UIWebView = 0
-    case WKWebView = 1
-  }
+	private enum Type: Int {
+		case UIWebView = 0
+		case WKWebView = 1
+	}
 
-  private let request = NSURLRequest(URL: NSBundle.mainBundle().revealWebsiteURL)
-  private var currentWebView: UIView?
-  private var topConstraint: NSLayoutConstraint?
+	private let request = NSURLRequest(URL: NSBundle.mainBundle().revealWebsiteURL)
+	private var currentWebView: UIView?
+	private var topConstraint: NSLayoutConstraint?
 
-  private lazy var uiWebView: UIWebView = {
-    let webView = UIWebView()
-    webView.delegate = self
-    webView.loadRequest(self.request)
-    return webView
-  }()
+	private lazy var uiWebView: UIWebView = {
+		let webView = UIWebView()
+		webView.delegate = self
+		webView.loadRequest(self.request)
+		return webView
+	}()
 
-  private lazy var wkWebView: WKWebView = {
-    let webView = WKWebView()
-    webView.navigationDelegate = self
-    webView.loadRequest(self.request)
-    return webView
-  }()
+	private lazy var wkWebView: WKWebView = {
+		let webView = WKWebView()
+		webView.navigationDelegate = self
+		webView.loadRequest(self.request)
+		return webView
+	}()
 
-  @IBAction private func segmentedControlValueChanged(sender: UISegmentedControl) {
-    let nextWebView = sender.selectedSegmentIndex == Type.UIWebView.rawValue ? self.uiWebView : self.wkWebView
-    self.currentWebView?.removeFromSuperview()
-    self.setupWebView(nextWebView)
-  }
+	@IBAction private func segmentedControlValueChanged(sender: UISegmentedControl) {
+		let nextWebView = sender.selectedSegmentIndex == Type.UIWebView.rawValue ? self.uiWebView : self.wkWebView
+		self.currentWebView?.removeFromSuperview()
+		self.setupWebView(nextWebView)
+	}
 
-  private func setupWebView(webView: UIView) {
-    webView.translatesAutoresizingMaskIntoConstraints = false
+	private func setupWebView(webView: UIView) {
+		webView.translatesAutoresizingMaskIntoConstraints = false
 
-    let leftConstraint = NSLayoutConstraint(item: webView, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1, constant: 0)
-    let rightConstraint = NSLayoutConstraint(item: webView, attribute: .Right, relatedBy: .Equal, toItem: self.view, attribute: .Right, multiplier: 1, constant: 0)
-    let bottomConstraint = NSLayoutConstraint(item: webView, attribute: .Bottom, relatedBy: .Equal, toItem: self.bottomLayoutGuide, attribute: .Top, multiplier: 1, constant: 0)
-    let topConstraint = NSLayoutConstraint(item: webView, attribute: .Top, relatedBy: .Equal, toItem: self.topLayoutGuide, attribute: .Bottom, multiplier: 1, constant: 0)
+		let leftConstraint = NSLayoutConstraint(item: webView, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1, constant: 0)
+		let rightConstraint = NSLayoutConstraint(item: webView, attribute: .Right, relatedBy: .Equal, toItem: self.view, attribute: .Right, multiplier: 1, constant: 0)
+		let bottomConstraint = NSLayoutConstraint(item: webView, attribute: .Bottom, relatedBy: .Equal, toItem: self.bottomLayoutGuide, attribute: .Top, multiplier: 1, constant: 0)
+		let topConstraint = NSLayoutConstraint(item: webView, attribute: .Top, relatedBy: .Equal, toItem: self.topLayoutGuide, attribute: .Bottom, multiplier: 1, constant: 0)
 
-    self.currentWebView = webView
-    self.view.addSubview(webView)
-    self.view.addConstraints([leftConstraint, rightConstraint, topConstraint, bottomConstraint])
-    self.topConstraint = topConstraint
-  }
+		self.currentWebView = webView
+		self.view.addSubview(webView)
+		self.view.addConstraints([leftConstraint, rightConstraint, topConstraint, bottomConstraint])
+		self.topConstraint = topConstraint
+	}
 
-  private func showFailAlert() {
-    let alertViewController = UIAlertController(
-      title: NSLocalizedString("Error", comment: "Alert title on content failed loading"),
-      message: NSLocalizedString("Failed to load content. Make sure you're connected to the internet and try again.", comment: "Alert message on content failed loading"),
-      preferredStyle: .Alert
-    )
-    
-    alertViewController.addAction(UIAlertAction(
-      title: NSLocalizedString("Ok", comment: "Alert dismiss button on content failed loading"),
-      style: .Cancel,
-      handler: nil)
-    )
-    
-    alertViewController.popoverPresentationController?.sourceView = self.view
-    self.presentViewController(alertViewController, animated: true, completion: nil)
-  }
+	private func showFailAlert() {
+		let alertViewController = UIAlertController(
+			title: NSLocalizedString("Error", comment: "Alert title on content failed loading"),
+			message: NSLocalizedString("Failed to load content. Make sure you're connected to the internet and try again.", comment: "Alert message on content failed loading"),
+			preferredStyle: .Alert
+		)
+
+		alertViewController.addAction(UIAlertAction(
+			title: NSLocalizedString("Ok", comment: "Alert dismiss button on content failed loading"),
+			style: .Cancel,
+			handler: nil)
+		)
+
+		alertViewController.popoverPresentationController?.sourceView = self.view
+		self.presentViewController(alertViewController, animated: true, completion: nil)
+	}
 }
 
 // MARK:- UIWebViewDelegate
 extension WebViewController: UIWebViewDelegate {
-  func webViewDidStartLoad(webView: UIWebView) {
-    UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-  }
+	func webViewDidStartLoad(webView: UIWebView) {
+		UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+	}
 
-  func webViewDidFinishLoad(webView: UIWebView) {
-    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-  }
+	func webViewDidFinishLoad(webView: UIWebView) {
+		UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+	}
 }
 
 // MARK:- WKNavigationDelegate
 extension WebViewController: WKNavigationDelegate {
-  func webView(webView: WKWebView, didCommitNavigation navigation: WKNavigation!) {
-    UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-  }
+	func webView(webView: WKWebView, didCommitNavigation navigation: WKNavigation!) {
+		UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+	}
 
-  func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
-    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-  }
+	func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+		UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+	}
 
-  func webView(webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: NSError) {
-    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-    self.showFailAlert()
-  }
+	func webView(webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: NSError) {
+		UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+		self.showFailAlert()
+	}
 
-  func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
-    UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-    self.showFailAlert()
-  }
+	func webView(webView: WKWebView, didFailNavigation navigation: WKNavigation!, withError error: NSError) {
+		UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+		self.showFailAlert()
+	}
 }
