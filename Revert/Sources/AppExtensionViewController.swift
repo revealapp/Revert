5928539e39f459d145fa4c3bb4feac1fa0e7dde7
nabby26 <greenybella@yahoo.com>
2020-11-09 15:09:35 +1100

    Add description label on App Extension screen

--- Revert/Sources/AppExtensionViewController.swift
+++ Revert/Sources/AppExtensionViewController.swift
@@ -3,6 +3,13 @@
 import Foundation
 
 final class AppExtensionViewController: RevertViewController {
+  @IBOutlet var descriptionLabel: UILabel!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    descriptionLabel.text = " Tap the activity button and choose Revert App Extension activity item.\nThe App Extension will be launched and appears as a different connection when the screen is inspected in Reveal app"
+  }
 
   @IBAction func shareButtonTapped(_ sender: UIBarButtonItem) {
     guard let image = UIImage(named: "info_icon") else { return }
